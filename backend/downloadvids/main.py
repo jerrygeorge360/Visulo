d2571f729ac31a8223d613c46428e226674aae3f
jerry george <jerrygithub360@gmail.com>
2024-12-01 02:57:29 +0100

    added cookie.txt

--- backend/downloadvids/main.py
+++ backend/downloadvids/main.py
@@ -45,7 +45,7 @@ class Videos(VideosMeta):
             unique_folder = os.path.join(self.output_folder, video_id)
             os.makedirs(unique_folder, exist_ok=True)
             ydl_opts = {
-                'cookiefile': 'cookie.json',
+                'cookiefile': 'cookies.txt',
                 "outtmpl": os.path.join(unique_folder, "%(id)s.%(ext)s"),
                 "format": "best",
                 "writesubtitles": True,
